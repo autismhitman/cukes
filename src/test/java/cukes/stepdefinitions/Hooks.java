c81f60a1f9805d3555a1856750520d3690741ea6
videomaker318 <autismhitman@gmail.com>
2025-04-14 18:35:49 +0530

    second

--- src/test/java/cukes/stepdefinitions/Hooks.java
+++ src/test/java/cukes/stepdefinitions/Hooks.java
@@ -11,13 +11,13 @@ public class Hooks {
 	public static void setup(Scenario scenario) {
 		
         Driver.initDriver(System.getProperty("browser","chrome"));
-        System.out.println(" Thread id is : " + Thread.currentThread().threadId()); 
+     //   System.out.println(" Thread id is : " + Thread.currentThread().threadId()); 
         System.out.println("Scenario is : "+ scenario.getName());
 	}
 	
 	 @After
 	public static void teardown(Scenario scenario) {
-		 System.out.println(" Thread id is : " + Thread.currentThread().threadId()); 
+	//	 System.out.println(" Thread id is : " + Thread.currentThread().threadId()); 
 		 System.out.println("Scenario is : "+ scenario.getName());
         Driver.closeDriver(); 
 

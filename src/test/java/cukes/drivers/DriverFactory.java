4361c7cab647d0dcfca4c722e6aae5c5c2c26596
videomaker318 <autismhitman@gmail.com>
2025-04-15 12:23:26 +0530

    third

--- src/test/java/cukes/drivers/DriverFactory.java
+++ src/test/java/cukes/drivers/DriverFactory.java
@@ -3,6 +3,9 @@ package cukes.drivers;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverFactory {
 	 
@@ -13,10 +16,18 @@ public class DriverFactory {
 		switch(browser) {
 		  
 		case "chrome" ->{
+			
+			 WebDriverManager.chromedriver().setup();
 			 driver = new ChromeDriver();
+			 
 			 }
 		case "edge" -> {
+			   WebDriverManager.edgedriver().setup();
 			   driver =new EdgeDriver();
+		   }
+		case "ff" -> {
+			   WebDriverManager.firefoxdriver().setup();
+			   driver =new FirefoxDriver();
 		   }
 		   default -> throw new IllegalStateException("INVALID BROWSER" + browser);
 		}
